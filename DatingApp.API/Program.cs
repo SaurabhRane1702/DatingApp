34bc51694808af5e580145f339d4152c713381a4
SaurabhRane1702 <63310412+SaurabhRane1702@users.noreply.github.com>
2020-05-20 14:51:43 +0530

    End Of Section 8

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,24 @@ namespace DatingApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var Services = scope.ServiceProvider;
+                try
+                {
+                    var context = Services.GetRequiredService<Data.DataContext>();
+                    context.Database.Migrate();
+                    Data.Seed.SeedUsers(context);
+                }
+                catch (System.Exception ex)
+                {
+                    var logger = Services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error occured during Migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
